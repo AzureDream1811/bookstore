6bb2182287c3201d7187acd0886083c63f474031
Phạm Đức Sang <23130271@st.hcmuaf.edu.vn>
2026-07-03 20:41:02 +0700

    UC_10. Thanh toán

--- src/main/java/com/bookstore/controller/AppController.java
+++ src/main/java/com/bookstore/controller/AppController.java
@@ -125,6 +125,11 @@ public class AppController {
             }
         }
     }
+    private PaymentController paymentController = new PaymentController();
+
+    public void checkout(Customer customer) {
+        paymentController.processPayment(customer);
+}
 }
 
 
