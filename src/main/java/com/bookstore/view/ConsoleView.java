f71a9574fe16ef1600d275e173be2ddb958d3e40
23130312-star <23130312@st.hcmuaf.edu.vn>
2026-07-04 00:25:36 +0700

    feat: UC20 vao view

--- src/main/java/com/bookstore/view/ConsoleView.java
+++ src/main/java/com/bookstore/view/ConsoleView.java
@@ -170,6 +170,8 @@ public class ConsoleView {
         print("1. Xem và tìm kiếm sách");
         print("2. Gio hang / hoa don");
         print("3. Quan ly ban hang");
+        print("4. Thue sach");
+        print("5. Tra sach");
         print("0. Dang xuat");
         return readInt("Chon chuc nang: ");
     }
