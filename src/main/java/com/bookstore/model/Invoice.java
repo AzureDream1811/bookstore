b83bb9315c913f63ef75652b2ba6d71921fc22a7
Phạm Đức Sang <23130271@st.hcmuaf.edu.vn>
2026-07-03 20:38:47 +0700

    UC_10. Thanh toán

---
+++ src/main/java/com/bookstore/model/Invoice.java
@@ -0,0 +1,28 @@
+package com.bookstore.model;
+
+import java.util.Date;
+
+public class Invoice {
+    private Order order;
+    private PaymentMethod paymentMethod;
+    private Date invoiceDate;
+    private double totalAmount;
+
+    // Getters & Setters
+    public Order getOrder() { return order; }
+    public void setOrder(Order order) { this.order = order; }
+    public PaymentMethod getPaymentMethod() { return paymentMethod; }
+    public void setPaymentMethod(PaymentMethod paymentMethod) { this.paymentMethod = paymentMethod; }
+    public Date getInvoiceDate() { return invoiceDate; }
+    public void setInvoiceDate(Date invoiceDate) { this.invoiceDate = invoiceDate; }
+    public double getTotalAmount() { return totalAmount; }
+    public void setTotalAmount(double totalAmount) { this.totalAmount = totalAmount; }
+
+    @Override
+    public String toString() {
+        return "Hóa đơn #" + (order != null ? order.getOrderId() : "N/A") +
+               "\nNgày: " + invoiceDate +
+               "\nPhương thức: " + paymentMethod.getDisplayName() +
+               "\nTổng tiền: " + String.format("%,.0f", totalAmount) + " VND";
+    }
+}
